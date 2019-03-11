e3ba5bf3ab6f85832454d8014682e7c52f0f5b4d
vagnersaller <vsaller@yahoo.com>
2019-03-10 23:37:12 -0300

    english

--- prob1.py
+++ prob1.py
@@ -1,15 +1,15 @@
-#Soma de multiplos de 3 ou 5 ate 1000
+#sum of multiple 3 or 5 until 1000
 
-def eh_multiplo(valor):
-    return valor%3==0 or valor%5==0
+def is_multiple(value):
+    return value%3==0 or value%5==0
 
-def acha_soma(n):
+def find_sum(n):
     i=0
-    soma=0
+    _sum=0
     while i<n:
-        if eh_multiplo(i):
-            soma+=i
+        if is_multiple(i):
+            _sum+=i
         i+=1
-    return soma
+    return _sum
 
-print (acha_soma(1000))
+print (find_sum(1000))
